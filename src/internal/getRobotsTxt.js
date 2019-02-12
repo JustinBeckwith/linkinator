8feaa12fad0ebe11940096320e63988777c464a4
Justin Beckwith <beckwith@google.com>
2019-02-11 20:43:05 -0800

    refactor: use gaxios for robots

--- src/internal/getRobotsTxt.js
+++ src/internal/getRobotsTxt.js
@@ -1,26 +1,29 @@
 'use strict';
+
 const guard = require('robots-txt-guard');
 const parse = require('robots-txt-parse');
+const gaxios = require('gaxios');
 
-const bhttp = require('bhttp');
 const urllib = require('url');
 const urlobj = require('urlobj');
 
-function getRobotsTxt(url, options) {
+async function getRobotsTxt(url, options) {
   url = urlobj.parse(url);
   url.hash = null;
   url.path = url.pathname = '/robots.txt';
   url.query = null;
   url.search = null;
 
-  return bhttp
-    .get(urllib.format(url), {
-      discardResponse: true,
-      headers: { 'user-agent': options.userAgent },
-      stream: true
-    })
-    .then(parse)
-    .then(guard);
+  const res = await gaxios.request({
+    url: urllib.format(url),
+    headers: { 'user-agent': options.userAgent },
+    responseType: 'stream',
+    validateStatus: () => true
+  });
+  let txt;
+  txt = await parse(res.data);
+  txt = await guard(txt);
+  return txt;
 }
 
 module.exports = getRobotsTxt;
