40d702978b3448fabdb96dcc150a2d7e9a55bbf9
Justin Beckwith <justin.beckwith@gmail.com>
2020-12-01 19:07:15 -0800

    fix: remove express dependency

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import {EventEmitter} from 'events';
 import * as gaxios from 'gaxios';
 import * as http from 'http';
 import enableDestroy = require('server-destroy');
-import * as express from 'express';
+import finalhandler = require('finalhandler');
+import serveStatic = require('serve-static');
 import * as fs from 'fs';
 import * as util from 'util';
 import * as path from 'path';
@@ -201,27 +202,29 @@ export class LinkChecker extends EventEmitter {
    * @returns Promise that resolves with the instance of the HTTP server
    */
   private async startWebServer(root: string, port: number, markdown?: boolean) {
-    const app = express()
-      .use(async (req, res, next) => {
-        if (!markdown) {
-          return next();
-        }
-        const pathParts = req.path.split('/').filter(x => !!x);
-        const ext = path.extname(pathParts[pathParts.length - 1]);
-        if (ext.toLowerCase() === '.md') {
-          const filePath = path.join(path.resolve(root), req.path);
-          const data = await readFile(filePath, {encoding: 'utf-8'});
-          const result = marked(data, {gfm: true});
-          res.send(result).end();
-          return;
-        }
-        return next();
-      })
-      .use(express.static(path.resolve(root)));
-    const server = await new Promise<http.Server>(resolve => {
-      const s = app.listen(port, () => resolve(s));
+    return new Promise<http.Server>((resolve, reject) => {
+      const serve = serveStatic(root);
+      const server = http
+        .createServer(async (req, res) => {
+          const pathParts = req.url!.split('/').filter(x => !!x);
+          if (pathParts.length > 0) {
+            const ext = path.extname(pathParts[pathParts.length - 1]);
+            if (markdown && ext.toLowerCase() === '.md') {
+              const filePath = path.join(path.resolve(root), req.url!);
+              const data = await readFile(filePath, {encoding: 'utf-8'});
+              const result = marked(data, {gfm: true});
+              res.writeHead(200, {
+                'content-type': 'text/html',
+              });
+              res.end(result);
+              return;
+            }
+          }
+          return serve(req, res, finalhandler(req, res) as () => void);
+        })
+        .listen(port, () => resolve(server))
+        .on('error', reject);
     });
-    return server;
   }
 
   /**
