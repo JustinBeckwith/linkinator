47e8cae2eb281f9e4f5db5fbb67892dd87ebb719
bencoe@google.com <bencoe@google.com>
2021-11-12 23:22:32 +0000

    feat: add cleanup setInterval()

--- src/queue.ts
+++ src/queue.ts
@@ -27,6 +27,13 @@ export class Queue extends EventEmitter {
   constructor(options: QueueOptions) {
     super();
     this.concurrency = options.concurrency;
+    // It was noticed in test that setTimeout() could sometimes trigger an event
+    // moments before it was scheduled. This leads to a delta between timeToRun
+    // and Date.now(), and a link may never crawl. This setInterval() ensures
+    // these items are eventually processed.
+    setInterval(() => {
+      if (this.activeFunctions === 0) this.tick();
+    }, 2500).unref();
   }
 
   add(fn: AsyncFunction, options?: QueueItemOptions) {
@@ -53,15 +60,8 @@ export class Queue extends EventEmitter {
       }
       // grab the element at the front of the array
       const item = this.q.shift()!;
-      // Depending on CPU load and other factors setTimeout() is not guranteed to run exactly
-      // when scheduled. This causes problems if there is only one item in the queue, as
-      // there's a chance it will never be processed. Allow for a small delta to address this:
-      const delta = 150;
-      const readyToExecute =
-        Math.abs(item.timeToRun - Date.now()) < delta ||
-        item.timeToRun < Date.now();
       // make sure this element is ready to execute - if not, to the back of the stack
-      if (readyToExecute) {
+      if (item.timeToRun <= Date.now()) {
         // this function is ready to go!
         this.activeFunctions++;
         item.fn().finally(() => {
