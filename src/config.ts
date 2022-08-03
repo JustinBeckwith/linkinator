ee023b020dca3c876a7472f89c12422ff55081e0
Anshul Sahni <anshul_sahni@live.com>
2022-08-04 01:18:53 +0530

    feat(config): Added base for adding multi-file config

--- src/config.ts
+++ src/config.ts
@@ -23,23 +23,7 @@ export interface Flags {
 export async function getConfig(flags: Flags) {
   // check to see if a config file path was passed
   const configPath = flags.config || 'linkinator.config.json';
-  let configData: string | undefined;
-  try {
-    configData = await fs.readFile(configPath, {encoding: 'utf8'});
-  } catch (e) {
-    if (flags.config) {
-      console.error(`Unable to find config file ${flags.config}`);
-      throw e;
-    }
-  }
-
-  let config: Flags = {};
-  if (configData) {
-    config = JSON.parse(configData);
-  }
-  // `meow` is set up to pass boolean flags as `undefined` if not passed.
-  // copy the struct, and delete properties that are `undefined` so the merge
-  // doesn't blast away config level settings.
+  let config: Flags = await parseConfigFile(configPath);
   const strippedFlags = Object.assign({}, flags);
   Object.entries(strippedFlags).forEach(([key, value]) => {
     if (
@@ -54,4 +38,48 @@ export async function getConfig(flags: Flags) {
   // with CLI flags getting precedence
   config = Object.assign({}, config, strippedFlags);
   return config;
+}
+
+type ConfigTypes = 'js' | 'json';
+type ConfigParser = (configPath: string) => Promise<Flags>;
+
+const configParserMap: Record<ConfigTypes, ConfigParser> = {
+  js: parseJsConfigFile,
+  json: parseJsonConfigFile,
+};
+
+function parseConfigFile(configPath: string): Promise<Flags> {
+  const typeOfConfig = getTypeOfConfig(configPath);
+
+  const configParser = configParserMap[typeOfConfig];
+
+  return configParser(configPath);
+}
+
+function getTypeOfConfig(configPath: string): ConfigTypes {
+  const configFileExtension: string = configPath.slice(
+    configPath.lastIndexOf('.') + 1
+  );
+
+  if (['js', 'json'].includes(configFileExtension)) {
+    return configFileExtension as ConfigTypes;
+  }
+
+  throw new Error('Config file should be either of js, json');
+}
+
+async function parseJsConfigFile(configPath: string): Promise<Flags> {
+  return {};
+}
+
+async function parseJsonConfigFile(configPath: string): Promise<Flags> {
+  try {
+    const configFileContents: string = await fs.readFile(configPath, {
+      encoding: 'utf-8',
+    });
+    return JSON.parse(configFileContents);
+  } catch (e) {
+    console.error(`Unable to read or parse the JSON config file ${configPath}`);
+    throw e;
+  }
 }
