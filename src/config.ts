f0bb1bd21808ab3fa405685c8dfdce6b8cda95fb
Anshul Sahni <anshul.sahni@razorpay.com>
2022-08-08 00:42:14 +0530

    feat(Config):Adding support for diff kinds of JS file

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import {promises as fs} from 'fs';
+import {createRequire} from 'module';
 
 export interface Flags {
   concurrency?: number;
@@ -20,10 +21,16 @@ export interface Flags {
   urlRewriteReplace?: string;
 }
 
+const validConfigExtensions = ['js', 'mjs', 'cjs','json'];
+
 export async function getConfig(flags: Flags) {
   // check to see if a config file path was passed
   const configPath = flags.config || 'linkinator.config.json';
-  let config: Flags = await parseConfigFile(configPath);
+  let config: Flags = {};
+
+  if (flags.config) {
+    config = await parseConfigFile(configPath);
+  }
   const strippedFlags = Object.assign({}, flags);
   Object.entries(strippedFlags).forEach(([key, value]) => {
     if (
@@ -40,36 +47,60 @@ export async function getConfig(flags: Flags) {
   return config;
 }
 
-type ConfigTypes = 'js' | 'json';
+type ConfigTypes = typeof validConfigExtensions[number];
 type ConfigParser = (configPath: string) => Promise<Flags>;
 
 const configParserMap: Record<ConfigTypes, ConfigParser> = {
   js: parseJsConfigFile,
   json: parseJsonConfigFile,
+  mjs: parseModuleJsConfigFile,
+  cjs: parseCommonJsConfigFile,
 };
 
-function parseConfigFile(configPath: string): Promise<Flags> {
+async function parseConfigFile(configPath: string): Promise<Flags> {
   const typeOfConfig = getTypeOfConfig(configPath);
 
   const configParser = configParserMap[typeOfConfig];
 
-  return configParser(configPath);
+  if (configParser) {
+    return configParser(configPath);
+  }
+
+  throw new Error(`Config file ${configPath} is invalid`);
 }
 
 function getTypeOfConfig(configPath: string): ConfigTypes {
-  const configFileExtension: string = configPath.slice(
-    configPath.lastIndexOf('.') + 1
-  );
+  const lastDotIndex = configPath.lastIndexOf('.');
 
-  if (['js', 'json'].includes(configFileExtension)) {
+  // Returning json in case file doesn't have an extension for backward compatibility
+  if (lastDotIndex === -1) return 'json';
+
+  const configFileExtension: string = configPath.slice(lastDotIndex + 1);
+
+  if (validConfigExtensions.includes(configFileExtension)) {
     return configFileExtension as ConfigTypes;
   }
 
   throw new Error('Config file should be either of js, json');
 }
 
+async function parseModuleJsConfigFile(configPath: string): Promise<Flags> {
+  const config = (await import(configPath)).default;
+
+  return config;
+}
+
+async function parseCommonJsConfigFile(configPath: string): Promise<Flags> {
+  const require = createRequire(import.meta.url);
+  const config = require(configPath);
+  return config;
+}
+
 async function parseJsConfigFile(configPath: string): Promise<Flags> {
-  return {};
+  // TODO: Added support for both commonjs & ES Modules
+  const config = (await import(configPath)).default;
+
+  return config;
 }
 
 async function parseJsonConfigFile(configPath: string): Promise<Flags> {
