929caa7e776ed22393a2437364d502925322563e
Justin Beckwith <justin.beckwith@discordapp.com>
2024-07-01 14:15:00 -0400

    feat: add option for setting user agent (#612)

--- src/options.ts
+++ src/options.ts
@@ -23,12 +23,16 @@ export type CheckOptions = {
 	retryErrorsCount?: number;
 	retryErrorsJitter?: number;
 	urlRewriteExpressions?: UrlRewriteExpression[];
+	userAgent?: string;
 };
 
 export type InternalCheckOptions = {
 	syntheticServerRoot?: string;
 	staticHttpServerHost?: string;
 } & CheckOptions;
+
+export const DEFAULT_USER_AGENT =
+	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36';
 
 /**
  * Validate the provided flags all work with each other.
@@ -75,6 +79,7 @@ export async function processOptions(
 		);
 	}
 
+	options.userAgent = options.userAgent ?? DEFAULT_USER_AGENT;
 	options.serverRoot &&= path.normalize(options.serverRoot);
 
 	// Expand globs into paths
