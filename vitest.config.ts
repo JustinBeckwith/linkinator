8b83501a051c5ba6c6615126fd6d81a7a14101d0
Justin Beckwith <justin.beckwith@gmail.com>
2025-07-01 18:09:47 -0400

    timeouts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['test/test.*.ts'],
-    testTimeout: 20_000,
+    testTimeout: 40_000,
     coverage: {
       provider: 'v8'
     },
