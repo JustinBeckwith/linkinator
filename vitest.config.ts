3ba6661d2b480b7cc721fb4c17d0df6b455f25e5
Justin Beckwith <justin.beckwith@gmail.com>
2025-07-07 16:52:57 -0700

    womp

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    // this is all for github actions being weird
+    pool: 'forks',
+    poolOptions: {
+      forks: {
+        singleFork: true
+      }
+    },
+
+    // normal setup
     setupFiles: ['./test/setup.ts'],
     include: ['test/test.*.ts'],
     testTimeout: 20_000,
