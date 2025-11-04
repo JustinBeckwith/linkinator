66767beefd9cb8df7d74ee269ef6450df3face6d
Justin Beckwith <justin.beckwith@gmail.com>
2025-11-04 07:50:07 -0800

    run the linter

--- test/test.insecure.ts
+++ test/test.insecure.ts
@@ -2,18 +2,18 @@ import { execa } from 'execa';
 import { assert, describe, it } from 'vitest';
 
 describe('cli', () => {
-  it('should allow insecure certs', async () => {
-    const response = await execa(
-      'node',
-      [
-        'build/src/cli.js',
-        'https://self-signed.badssl.com/',
-        '--allow-insecure-certs',
-      ],
-      {
-        reject: false,
-      }
-    );
-    assert.match(response.stderr, /Successfully scanned/);
-  });
+	it('should allow insecure certs', async () => {
+		const response = await execa(
+			'node',
+			[
+				'build/src/cli.js',
+				'https://self-signed.badssl.com/',
+				'--allow-insecure-certs',
+			],
+			{
+				reject: false,
+			},
+		);
+		assert.match(response.stderr, /Successfully scanned/);
+	});
 });
