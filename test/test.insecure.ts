7bcc536e5307c31067ccfb9d7679402801cffebc
Justin Beckwith <justin.beckwith@gmail.com>
2025-11-04 07:47:19 -0800

    feat: add test for --allow-insecure-certs

---
+++ test/test.insecure.ts
@@ -0,0 +1,19 @@
+import { execa } from 'execa';
+import { assert, describe, it } from 'vitest';
+
+describe('cli', () => {
+  it('should allow insecure certs', async () => {
+    const response = await execa(
+      'node',
+      [
+        'build/src/cli.js',
+        'https://self-signed.badssl.com/',
+        '--allow-insecure-certs',
+      ],
+      {
+        reject: false,
+      }
+    );
+    assert.match(response.stderr, /Successfully scanned/);
+  });
+});
